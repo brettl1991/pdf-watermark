7a7a138aca4d6a5fb6afdd8f8ffcdb8255f0421f
Agnes <brettl1991@gmail.com>
2021-04-16 14:07:48 +0100

    Creating the new file with watermark on each page

--- pdf.py
+++ pdf.py
@@ -11,3 +11,6 @@ for i in range(template.getNumPages()):
   page = template.getPage(i)
   page.mergePage(watermark.getPage(0))
   output.addPage(page)
+
+with open ('watermarked_output.pdf', 'wb') as file:
+  output.write(file)
