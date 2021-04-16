43b42f4f6ef00013f7cf34fe0441b5f5c5db1991
Agnes <brettl1991@gmail.com>
2021-04-16 13:57:46 +0100

    Fixing type error

--- pdf.py
+++ pdf.py
@@ -8,4 +8,5 @@ watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
 output = PyPDF2.PdfFileWriter()
 
 for i in range(template.getNumPages()):
-  page = template.getPages(i)
+  page = template.getPage(i)
+  
